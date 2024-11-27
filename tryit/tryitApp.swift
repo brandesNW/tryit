b65be92148860fe7f1ef999ff2a847f8da5e4ff4
Aaron Brandes <aaron.brandes@northwestern.edu>
2024-11-27 12:35:41 -0500

    Initial Commit

---
+++ tryit/tryitApp.swift
@@ -0,0 +1,17 @@
+//
+//  tryitApp.swift
+//  tryit
+//
+//  Created by Aaron Andre Brandes on 11/27/24.
+//
+
+import SwiftUI
+
+@main
+struct tryitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tryitTests/tryitTests.swift
@@ -0,0 +1,17 @@
+//
+//  tryitTests.swift
+//  tryitTests
+//
+//  Created by Aaron Andre Brandes on 11/27/24.
+//
+
+import Testing
+@testable import tryit
+
+struct tryitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
